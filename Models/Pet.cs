750616027f70629c4c10b4bbc0d301d1613918f4
fraol <phrolaye@gmail.com>
2023-09-21 12:08:00 -0500

    pet owner

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,7 +5,33 @@ using System;
 
 namespace pet_hotel
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType {Shepherd, Poodle, Beagle, Bulldog, Terrier, Boxer, Labrador, Retriever}
+    public enum PetColorType {White, Black, Golden, Tricolor, Spotted}
+    public class Pet {
+        public string name {get;set;}
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetColorType color {get;set;}
+        public DateTime checkedInAt {get;set;}  
+        public int petOwnerid {get;set;}
+        public int id {get;set;}
+        public PetBreedType breed {get;set;}
+        [ForeignKey("petOwner")]
+        public int petOwnerId {get;set;}
+        public PetOwner petOwner {get;set;}
+    }
+}
+
+{
+  "name" : "Fido",
+  "color" : "White",
+  "checkedInAt" : "2020-07-21T03:17:58.917069",
+  "petOwnerid" : 1,
+  "id" : 1,
+  "breed" : "Beagle",
+  "petOwner" : {
+      "id" : 1,
+      "petCount" : 1,
+      "emailAddress" : "asdf@asdf.com",
+      "name" : "blaine"
+  }
 }
